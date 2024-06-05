86af1113a9b0931f70088008e1ca19f2455668a6
Gerson <pinedagerson1993@gmail.com>
2024-06-05 19:49:37 -0400

    Mejoras en version movil

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../ui/auth/LoginForm";
 
 export default function Page() {
   return (
-    <div className="bg-white p-4 md:py-8 w-full md:w-[425px] m-auto md:my-4 md:border md:rounded-md md:shadow-xl">
+    <div className="bg-white px-10 md:py-8 w-full md:w-[425px] m-auto md:my-4 md:border md:rounded-md md:shadow-xl">
         <h2 className="text-center text-2xl font-bold text-blue-500 mb-4">
           Iniciar sesión
         </h2>
